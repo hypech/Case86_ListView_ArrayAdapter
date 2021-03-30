994ce06fa805d21f77dd62193126a7d4fcb8d806
HYPech <TCTORBAO@GMAIL.COM>
2021-03-29 23:01:27 -0400

    arrayadapter

--- app/src/main/java/com/hypech/case86_listview_arrayadapter/MainActivity.java
+++ app/src/main/java/com/hypech/case86_listview_arrayadapter/MainActivity.java
@@ -3,12 +3,36 @@ package com.hypech.case86_listview_arrayadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] nameArray = {"Octopus","Pig","Sheep","Rabbit","Snake","Spider" };
+
+    String[] infoArray = {
+            "8 tentacled monster",
+            "Delicious in rolls",
+            "Great for jumpers",
+            "Nice in a stew",
+            "Great for shoes",
+            "Scary."
+    };
+
+    Integer[] imageArray = {R.drawable.icon7,
+            R.drawable.icon1,
+            R.drawable.icon2,
+            R.drawable.icon3,
+            R.drawable.icon4,
+            R.drawable.icon5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomListAdapter whatever = new CustomListAdapter(this, nameArray, infoArray, imageArray);
+        listView = (ListView) findViewById(R.id.listviewID);
+        listView.setAdapter(whatever);
     }
 }
